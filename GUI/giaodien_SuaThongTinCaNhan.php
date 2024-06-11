83cb2174e1fd471aba50531133efe546957cd601
Icegozi <haxuanphuc48@gmail.com>
2024-06-11 13:56:01 +0700

    csdl

--- GUI/giaodien_SuaThongTinCaNhan.php
+++ GUI/giaodien_SuaThongTinCaNhan.php
@@ -13,7 +13,9 @@
     <!-- CSS FILES -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Sono:wght@200;300;400;500;700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Sono:wght@200;300;400;500;700&display=swap"
+        rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -21,95 +23,98 @@
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link href="css/templatemo-pod-talk.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/5.5.2/collection/components/icon/icon.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/5.5.2/collection/components/icon/icon.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ionicons/5.5.2/ionicons.js"></script>
 
     <style>
-        body {
-            font-family: 'Times New Roman', Times, serif;
-            margin: 0;
-            padding: 0;
-        }
+    body {
+        font-family: 'Times New Roman', Times, serif;
+        margin: 0;
+        padding: 0;
+    }
 
-        .container {
-            max-width: 1000px;
-            margin: 0 auto;
-        }
+    .container {
+        max-width: 1000px;
+        margin: 0 auto;
+    }
 
-        h2 {
-            text-align: center;
-            margin-bottom: 20px;
-        }
+    h2 {
+        text-align: center;
+        margin-bottom: 20px;
+    }
 
-        .form-group {
-            margin-bottom: 15px;
-        }
+    .form-group {
+        margin-bottom: 15px;
+    }
 
-        .form-control {
-            padding: 5px 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            font-size: 20px;
-        }
+    .form-control {
+        padding: 5px 20px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        font-size: 20px;
+    }
 
-        .form-control[readonly] {
-            background-color: #e9ecef;
-            opacity: 1;
-        }
+    .form-control[readonly] {
+        background-color: #e9ecef;
+        opacity: 1;
+    }
 
-        .btn {
-            background-color: #007bff;
-            color: white;
-            padding: 12px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            width: 100%;
-        }
+    .btn {
+        background-color: #007bff;
+        color: white;
+        padding: 12px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        width: 100%;
+    }
 
-        .img-fluid {
-            max-width: 65%;
-            height: 50%;
-            border-radius: 50%;
-            margin-left: 20%;
-        }
+    .img-fluid {
+        max-width: 65%;
+        height: 50%;
+        border-radius: 50%;
+        margin-left: 20%;
+    }
 
-        .title {
-            margin-left: 28%;
-        }
+    .title {
+        margin-left: 28%;
+    }
 
-        #content {
-            width: 100%;
-            height: 31%;
-            background-color: none;
-            border: none;
-            text-align: center;
-            color: black;
-            font-family: "Shadows Into Light", cursive;
-            font-weight: 500;
-            font-style: normal;
-            font-size: 24px;
-        }
+    #content {
+        width: 100%;
+        height: 31%;
+        background-color: none;
+        border: none;
+        text-align: center;
+        color: black;
+        font-family: "Shadows Into Light", cursive;
+        font-weight: 500;
+        font-style: normal;
+        font-size: 24px;
+    }
 
-        .col-md-6 label {
-            font-weight: bold;
-            font-size: larger;
-        }
+    .col-md-6 label {
+        font-weight: bold;
+        font-size: larger;
+    }
 
-        .password-wrapper{
-            display: flex;
-            flex-wrap: wrap;
-        }
-        .password-wrapper ion-icon {
-            margin-top: 32px;
-            margin-left: 15px;
-            font-size: 30px;
-            color: #6c757d;
-            cursor: pointer;
-        }
-        .password-wrapper .form-control{
-            padding-right: 150px;
-        }
+    .password-wrapper {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .password-wrapper ion-icon {
+        margin-top: 32px;
+        margin-left: 15px;
+        font-size: 30px;
+        color: #6c757d;
+        cursor: pointer;
+    }
+
+    .password-wrapper .form-control {
+        padding-right: 150px;
+    }
     </style>
 
 </head>
@@ -129,38 +134,48 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <img src="images/medium-shot-young-people-recording-podcast.jpg" alt="Hình ảnh" class="img-fluid"><br>
+                        <img src="images/medium-shot-young-people-recording-podcast.jpg" alt="Hình ảnh"
+                            class="img-fluid"><br>
                         <h4 class="title">Hà Xuân Phúc</h4>
-                        <textarea name="content" id="content" readonly>Hiện tại là quá khứ của tương lai, hôm nay là bức hình của khoảnh khắc</textarea>
+                        <textarea name="content" id="content"
+                            readonly>Hiện tại là quá khứ của tương lai, hôm nay là bức hình của khoảnh khắc</textarea>
                     </div>
                     <div class="col-md-6">
                         <form action="" method="post">
                             <div class="form-group">
                                 <label for="tenKhachHang">Full name</label>
-                                <input type="text" class="form-control" id="tenKhachHang" name="tenKhachHang" value="Hà Xuân Phúc">
+                                <input type="text" class="form-control" id="tenKhachHang" name="tenKhachHang"
+                                    value="Hà Xuân Phúc">
                             </div>
                             <div class="form-group">
                                 <label for="tenTaiKhoan">User name</label>
-                                <input type="text" class="form-control" id="tenTaiKhoan" name="tenTaiKhoan" value="Icegozi">
+                                <input type="text" class="form-control" id="tenTaiKhoan" name="tenTaiKhoan"
+                                    value="Icegozi">
                             </div>
-                             <div class="form-group password-wrapper">
+                            <div class="form-group password-wrapper">
                                 <div class="">
                                     <label for="matKhau">Password:</label>
-                                    <input disabled type="password" class="form-control" id="matKhau" name="matKhau" value="123456789">
+                                    <input disabled type="password" class="form-control" id="matKhau" name="matKhau"
+                                        value="123456789">
                                 </div>
-                                <a href="#"><ion-icon name="hammer-outline"></ion-icon></a>
+                                <a href="#">
+                                    <ion-icon name="hammer-outline"></ion-icon>
+                                </a>
                             </div>
                             <div class="form-group">
                                 <label for="soDienThoai">Phone number</label>
-                                <input type="text" class="form-control" id="soDienThoai" name="soDienThoai" value="0964203698">
+                                <input type="text" class="form-control" id="soDienThoai" name="soDienThoai"
+                                    value="0964203698">
                             </div>
                             <div class="form-group">
                                 <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" name="email" value="sinhvien2k3@gmail.com">
+                                <input type="email" class="form-control" id="email" name="email"
+                                    value="sinhvien2k3@gmail.com">
                             </div>
                             <div class="form-group">
                                 <label for="diaChi">Address</label>
-                                <input type="text" class="form-control" id="diaChi" name="diaChi" value="Ngõ 1, đường Hoàng Hoa thám, Hà Nội">
+                                <input type="text" class="form-control" id="diaChi" name="diaChi"
+                                    value="Ngõ 1, đường Hoàng Hoa thám, Hà Nội">
                             </div>
                             <button type="submit" class="btn btn-primary">Update</button>
                         </form>
