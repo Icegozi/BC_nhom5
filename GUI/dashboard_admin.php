959c9d32e428ca2a8670c142d2b40f9648652dc2
Icegozi <haxuanphuc48@gmail.com>
2024-06-20 23:41:28 +0700

    add usecase TaiKhoan

--- GUI/dashboard_admin.php
+++ GUI/dashboard_admin.php
@@ -1,5 +1,15 @@
 <?php
+session_start();
 include 'header_admin.php';
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 if(isset($_GET['pg'])){
     $pg = $_GET['pg'];
 
