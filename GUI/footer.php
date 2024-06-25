cae3f96e774130a938fb0c303dff6d71d81a66bd
Icegozi <haxuanphuc48@gmail.com>
2024-06-25 14:27:36 +0700

    add icon to footer

--- GUI/footer.php
+++ GUI/footer.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sample Online Store</title>
+    <title>Soccer field management</title>
     <link rel="stylesheet" href="css/footer.css?v= <?php echo time() ?>">
+    <link rel="stylesheet" href="css/footer.css?v= <?php echo time() ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body>
@@ -24,11 +26,13 @@
             <p>Phone: +1234567890</p>
         </div>
         <div class="social-icons">
-            <a href="#" target="_blank">Facebook</a>
-            <a href="#" target="_blank">Twitter</a>
-            <a href="#" target="_blank">Instagram</a>
-            <a href="#" target="_blank">LinkedIn</a>
+            <a href="https://www.facebook.com/icegozi" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://x.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com/ice_gozi/" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_google-one-tap-submit" target="_blank"><i
+                    class="fab fa-linkedin-in"></i></a>
         </div>
+
         <p>&copy; 2024 Sample Online Store. All rights reserved.</p>
     </footer>
 </body>
